34291b1e4d9873ff8f45a269b70eb70cecb773a7
Crystal Melting Dot <stresspassing@gmail.com>
2020-09-27 20:30:49 +0300

    multiline input support

--- insect.py
+++ insect.py
@@ -12,8 +12,21 @@ def call_insect(inpt:str):
         test_inpt = test_inpt.replace(i, '')
     if test_inpt.isdigit():
         return eval(inpt)
-    if executable:
-        result = Popen([executable, inpt], stdout=PIPE, stderr=PIPE, encoding='utf-8').communicate()
-        return result[0] or result[1]
-    else:
+    
+    if not executable:
         return 'Insect was not found'
+
+    if len(lines := inpt.split('\n')) > 1:
+        process = Popen(
+            [executable],
+            stdin=PIPE,
+            stdout=PIPE,
+            stderr=PIPE,
+            encoding='utf-8')
+        result = process.communicate(inpt+'\n')
+        return result[0] or result[1]
+
+    
+    result = Popen([executable, inpt], stdout=PIPE, stderr=PIPE, encoding='utf-8').communicate()
+    return result[0] or result[1]
+        
