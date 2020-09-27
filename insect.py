ea652f686f19663ed1fcbc7fe5ccc3d2267a9120
Crystal Melting Dot <stresspassing@gmail.com>
2020-09-27 19:26:10 +0300

    remove print from insect module

--- insect.py
+++ insect.py
@@ -7,7 +7,6 @@ executable = which('insect')
 def call_insect(inpt:str):
     if executable:
         result = Popen([executable, inpt], stdout=PIPE, stderr=PIPE, encoding='utf-8').communicate()
-        print(f'Result from insect: {result}')
         return result[0] or result[1]
     else:
         return 'Insect was not found'
