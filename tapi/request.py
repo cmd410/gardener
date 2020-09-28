3b2783954f89e9a616f72396f028b303c7a41fb6
Crystal Melting Dot <stresspassing@gmail.com>
2020-09-28 19:27:28 +0300

    better reading of http responce

--- tapi/request.py
+++ tapi/request.py
@@ -1,6 +1,7 @@
 import json
 from types import SimpleNamespace
 
+import errno
 from gevent import socket, ssl, sleep
 
 from .util import greenlet
@@ -40,24 +41,49 @@ def make_request(token: str, method: str, body=None) -> SimpleNamespace:
     request_url = api_url.format(method=method, token=token)
     host = api_url[8:api_url.find('/bot')]
     path = request_url[request_url.find('/bot'):]
-    s = f'POST {path} HTTP/1.1\r\n'
-    s += f'Host: {host}\r\n'
+    request = f'POST {path} HTTP/1.1\r\n'
+    request += f'Host: {host}\r\n'
     
     if body is not None:
         body_data = json.dumps(body)
-        s += f'Content-Length: {len(body_data)}\r\n'
-        s += 'Content-Type: application/json; charset=utf-8\r\n'
+        request += f'Content-Length: {len(body_data)}\r\n'
+        request += 'Content-Type: application/json; charset=utf-8\r\n'
     else:
         body_data = ''
     
-    s += f'\r\n'
-    s += body_data
+    request += f'\r\n'
+    request += body_data
+    
     with socket.create_connection((host, 443)) as tcp_socket:
         with ssl.wrap_socket(tcp_socket) as ssl_socket:
-            ssl_socket.send(s.encode())
-            data: bytes = ssl_socket.recv(1024*7000)
-            headers, body = data.split(b'\r\n'*2, maxsplit=1)
+            ssl_socket.send(request.encode())
+            data: bytes = b''
+            
+            # Reading headers
+            while True:
+                data += ssl_socket.recv(256)
+                if b'\r\n\r\n' in data:
+                    break
+            headers, body = data.split(b'\r\n\r\n', maxsplit=1)
+            
+            # Get content-length
+            content_length = 0
+            for i in headers.decode().split('\r\n'):
+                if ':' not in i:
+                    continue
+                header_name, value = i.split(':', maxsplit=1)
+                if header_name.lower().strip() == 'content-length':
+                    content_length = int(value.strip())
+                    break
+            
+            # Read the rest of the body
+            data_left = content_length - len(body)
+            if  data_left > 0:
+                body += ssl_socket.recv(data_left)
+            
             decoded_data: SimpleNamespace = decode_json_body(body)
             if not decoded_data.ok:
-                raise APIError(f'[{decoded_data.error_code}] {decoded_data.description}')
+                code = decoded_data.error_code
+                descr = decoded_data.description
+                raise APIError(f'''[{code}] {descr}''')
             return decoded_data
